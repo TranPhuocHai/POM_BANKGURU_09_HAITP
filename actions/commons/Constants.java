5eac9340d3a77f38ce6e53c669cfce7ba4682963
Tran Phuoc Hai <hai.gsd@gmail.com>
2019-05-27 19:45:36 +0700

    Update Constants for LiveGuruFrontEnd And BackEnd

--- actions/commons/Constants.java
+++ actions/commons/Constants.java
@@ -9,5 +9,10 @@ public class Constants {
 	public static final String TEST_APP_URL = "http://demo.guru99.com/v3/";
 	public static final String STAGING_APP_URL = "http://demo.guru99.com/v2/";
 	public static final String PRODUCTION_APP_URL = "http://demo.guru99.com/v1/";
+	
+	public static final String LIVE_GURU_FRONT_END = "http://live.guru99.com/";
+	public static final String LIVE_GURU_BACK_END = "http://live.guru99.com/index.php/backendlogin";
+	
+	
 
 }
